4734884f8d97795a078710eabe8449c6947aff61
Jamie Clarke <jamie.clarke@virblue.com>
2013-10-14 14:32:31 +0100

    Added sqlite-net-extensions project

---
+++ Sqlite/Cirrious.MvvmCross.Community.Plugins.Sqlite.WindowsStore/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Cirrious.MvvmCross.Community.Plugins.Sqlite.WindowsStore.Bootstrap
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Json.PluginLoader>
+    {
+    }
+}
